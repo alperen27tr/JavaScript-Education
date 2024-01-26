6f8519b487008fa5057a0ceb6fbe7c44d8f3d422
Alperen Erdoğan <139046262+alperen27tr@users.noreply.github.com>
2024-01-26 16:45:38 +0300

    Delete use_propmt.js

--- use_propmt.js
+++
@@ -1,5 +0,0 @@
-
-let username0=prompt("Kullanıcı Adınız: ")
-
-let greeting=document.querySelector("#greeting")
-greeting.innerHTML=`${greeting.innerHTML} <small style="color:red">${username0}<small>`
